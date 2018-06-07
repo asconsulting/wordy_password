5b0f0ff0506a3f96d47b4fb6cbd7b4ff832a80ff
asconsulting <andrew@andrewstevens.consulting>
2018-06-07 02:17:19 -0400

    Initial Commit

---
+++ system/modules/wordy_password/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+ 
+/**
+ * ASC - Wordy Password
+ *
+ * Copyright (C) 2018 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/wordy_password
+ * @link       https://andrewstevens.consulting
+ */
+
+ 
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	
+    'Asc\WordyPassword' 				=> 'system/modules/wordy_password/library/Asc/WordyPassword.php',
+));
---
+++ system/modules/wordy_password/config/config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/**
+ * ASC - Wordy Password
+ *
+ * Copyright (C) 2018 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/wordy_password
+ * @link       https://andrewstevens.consulting
+ */
